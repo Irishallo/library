9fd1ef00290b2558d97c2a7071456742ec7b33c5
Iris <iclinde@xs4all.nl>
2021-01-06 15:46:03 +0100

    started the add book portion

--- books.js
+++ books.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -7,3 +9,24 @@ function Book(title, author, pages, read) {
         return `${title} by ${author}, ${pages} pages, ${read}`
       }
   }
+
+function addBookToLibrary() {
+  // do stuff here
+  let bookName = document.getElementById("title").value;
+  let authorName = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = ""
+  var check = document.getElementsByName('read'); 
+              
+    for(i = 0; i < check.length; i++) { 
+        if(yesread.checked) {
+            read = 'read';
+        } else {
+        read = 'not yet read';
+    }}
+
+  myLibrary.push(new Book(bookName, authorName, pages, read));
+}
+
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener('click', addBookToLibrary);
